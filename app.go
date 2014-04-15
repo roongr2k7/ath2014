b1ba5ed4ded2a107c92b9c9e819b4d35f6381d29
Thanabodee <thanabodee@osdev.co.th>
2014-04-15 14:00:40 +0700

    remove mux and change import lib to github.com/varokas/lib

--- app.go
+++ app.go
@@ -1,24 +1,23 @@
 package main
 
 import (
-  "github.com/gorilla/mux"
-  "log"
-  "net/http"
-  "./lib"
+	"github.com/varokas/ath2014/lib"
+	"log"
+	"net/http"
 )
 
-
 func main() {
-  rtr := mux.NewRouter()
-  rtr.HandleFunc("/topics", topics)
-  rtr.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
-  http.Handle("/", rtr)
+	http.HandleFunc("/topics", topics)
+	http.Handle("/", http.FileServer(http.Dir("./static")))
 
-  log.Println("Listening...")
-  http.ListenAndServe(":3000", nil)
+	log.Println("Listening...")
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		panic(err.Error())
+	}
 }
 
 func topics(w http.ResponseWriter, r *http.Request) {
-  controller := lib.Controller{}
-  w.Write(controller.GetTopics())
+	controller := lib.Controller{}
+	w.Write(controller.GetTopics())
 }
