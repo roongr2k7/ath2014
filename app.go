2b8c9f53687a2a659d9743c6ee50f846c474eb32
Pitsanu Swangpheaw <pitsanu_s@hotmail.com>
2014-04-16 09:54:40 +0700

    change directory structure

--- app.go
+++ app.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	"com.agile66/ath2014/lib"
+	"./lib"
 	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/api/topics", topics)
-        http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
-        http.HandleFunc("/", serveIndex)
+  //http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+  http.Handle("/", http.FileServer(http.Dir("./webroot")))
 	log.Println("Listening...")
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		panic(err.Error())
 	}
-} 
+}
 
 func serveIndex(w http.ResponseWriter, r *http.Request) {
   http.ServeFile(w, r, "./static/index.html")
